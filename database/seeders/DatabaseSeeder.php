1417272df778d408dc317434a76d51170b3f8cc8
AmeliaMutiara <amellidia494@gmail.com>
2024-01-05 15:40:59 +0700

    menu supplier revisi done

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -21,7 +23,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ParentSedder::class,
             CategorySeeder::class,
-            WarehouseSeeder::class
+            WarehouseSeeder::class,
+            SupplierSeeder::class
         ]);
     }
 }
