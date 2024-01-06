bd245bfa0962269a61fdaa84fff4324baec0cd2c
AmeliaMutiara <amellidia494@gmail.com>
2024-01-06 10:49:44 +0700

    model, controller acctaccount

---
+++ app/DataTables/AcctAccountDataTable.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\DataTables;
+
+use App\Models\AcctAccount;
+use Illuminate\Database\Eloquent\Builder as QueryBuilder;
+use Yajra\DataTables\EloquentDataTable;
+use Yajra\DataTables\Html\Builder as HtmlBuilder;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Html\Editor\Editor;
+use Yajra\DataTables\Html\Editor\Fields;
+use Yajra\DataTables\Services\DataTable;
+
+class AcctAccountDataTable extends DataTable
+{
+    /**
+     * Build the DataTable class.
+     *
+     * @param QueryBuilder $query Results from query() method.
+     */
+    public function dataTable(QueryBuilder $query): EloquentDataTable
+    {
+        return (new EloquentDataTable($query))
+            ->addIndexColumn()
+            ->addColumn('action', 'content.AcctAccount.List._action-menu')
+            ->setRowId('id');
+    }
+
+    /**
+     * Get the query source of dataTable.
+     */
+    public function query(AcctAccount $model): QueryBuilder
+    {
+        return $model->newQuery();
+    }
+
+    /**
+     * Optional method if you want to use the html builder.
+     */
+    public function html(): HtmlBuilder
+    {
+        return $this->builder()
+                    ->setTableId('acctaccount-table')
+                    ->columns($this->getColumns())
+                    ->minifiedAjax()
+                    ->dom('Bflrtip')
+                    ->parameters(["lengthMenu" => [5, 10, 25, 50, 75, 100]])
+                    ->orderBy(0, 'asc')
+                    ->autoWidth(false)
+                    ->responsive()
+                    ->parameters(['scrollX' => true])
+                    ->buttons([Button::make('reload')]);
+    }
+
+    /**
+     * Get the dataTable columns definition.
+     */
+    public function getColumns(): array
+    {
+        return [
+            Column::make('account_id')->title(__('No'))->data('DT_RowIndex')->addClass('text-center')->width(10),
+            Column::make('account_code')->title('No Perkiraan'),
+            Column::make('account_name')->title('Nama Perkiraan'),
+            Column::make('account_group')->title('Golongan Perkiraan'),
+            Column::make('account_type_id')->title('Kelompok Perkiraan'),
+            Column::make('account_status')->title('D/K'),
+            Column::computed('action')->title('Aksi')
+                ->exportable(false)
+                ->printable(false)
+                ->width(60)
+                ->addClass('text-center'),
+        ];
+    }
+
+    /**
+     * Get the filename for export.
+     */
+    protected function filename(): string
+    {
+        return 'AcctAccount_' . date('YmdHis');
+    }
+}
---
+++ app/Http/Controllers/AcctAccountController.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AcctAccount;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+use App\DataTables\AcctAccountDataTable;
+
+class AcctAccountController extends Controller
+{
+    public function index(AcctAccountDataTable $table)
+    {
+        Session::forget('datases');
+        $data = AcctAccount::select('account_code', 'account_name', 'account_group', 'account_type_id', 'account_status', 'account_id')
+        ->where('company_id', Auth::user()->company_id)
+        ->get();
+        return $table->render('content.AcctAccount.List.index');
+    }
+
+    public function addAcctAccount()
+    {
+        $datases = Session::get('datases');
+        $status = array(
+            '0' => 'Debit',
+            '1' => 'Kredit'
+        );
+        $account_type = array(
+            '0' => 'NA - Neraca Aktif',
+            '1' => 'NP - Neraca Pasif',
+            '2' => 'RA - Rugi Laba (A)',
+            '3' => 'RP - Rugi Laba (b)',
+        );
+        return view('content.AcctAccount.Add.index', compact('datases', 'status', 'account_type'));
+    }
+
+    public function addElementsAcctAccount(Request $request)
+    {
+        $datases = Session::get('datases');
+        if(!$datases || $datases == '') {
+            $datases['account_code']        = '';
+            $datases['account_name']        = '';
+            $datases['account_group']       = '';
+            $datases['account_status']      = '';
+            $datases['account_type_id']     = '';
+        }
+        $datases[$request->name] = $request->value;
+        Session::put('datases', $datases);
+    }
+
+    public function processAddAcctAccount(Request $request)
+    {
+        $fields = $request->validate([
+            'account_code'      => 'required',
+            'account_name'      => 'required',
+            'account_group'     => 'required',
+            'account_status'    => 'required',
+            'account_type_id'   => 'required',
+        ]);
+        try {
+            DB::beginTransaction();
+            $data = AcctAccount::create([
+                'account_code'          => $fields['account_code'],
+                'account_name'          => $fields['account_name'],
+                'account_group'         => $fields['account_group'],
+                'account_default_status'=> $fields['account_status'],
+                'account_status'        => $fields['account_code'],
+                'account_type_id'       => $fields['account_code'],
+                'company_id'            => Auth::user()->company_id
+            ]);
+            DB::commit();
+            return redirect()->route('account.index')->with(['msg' => 'Berhasil Menambahkan Data Perkiraan', 'type' => 'success']);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            dd($e);
+            report($e);
+            return redirect()->route('account.add')->with(['msg' => 'Gagal Menambahkan Data Perkiraan', 'type' => 'danger']);
+        }
+    }
+
+    public function addResetAcctAccount()
+    {
+        Session::forget('datases');
+        return redirect()->back();
+    }
+
+    public function getStatus($account_status)
+    {
+        $status = array(
+            '0' => 'Debit',
+            '1' => 'Kredit'
+        );
+        return $status[$account_status];
+    }
+
+    public function getType($account_type_id)
+    {
+        $account_type = array(
+            '0' => 'NA - Neraca Aktif',
+            '1' => 'NP - Neraca Pasif',
+            '2' => 'RA - Rugi Laba (A)',
+            '3' => 'RP - Rugi Laba (b)',
+        );
+        return $account_type[$account_type_id];
+    }
+
+    public function editAcctAccount($account_id)
+    {
+        $data = AcctAccount::select('account_code', 'account_id', 'account_name', 'account_group', 'account_status', 'account_type_id')
+        ->where('company_id', Auth::user()->company_id)
+        ->where('account_id', $account_id)
+        ->first();
+        $status = array(
+            '0' => 'Debit',
+            '1' => 'Kredit'
+        );
+        $account_type = array(
+            '0' => 'NA - Neraca Aktif',
+            '1' => 'NP - Neraca Pasif',
+            '2' => 'RA - Rugi Laba (A)',
+            '3' => 'RP - Rugi Laba (b)',
+        );
+        return view('content.AcctAccount.Edit.index', compact('data', 'status', 'account_type'));
+    }
+
+    public function processEditAcctAccount(Request $request)
+    {
+        $fields = $request->validate([
+            'account_id'        => '',
+            'account_code'      => 'required',
+            'account_name'      => 'required',
+            'account_group'     => 'required',
+            'account_status'    => 'required',
+            'account_type_id'   => 'required'
+        ]);
+        try {
+            DB::beginTransaction();
+            $table                              = AcctAccount::findOrFail($fields['account_id']);
+            $table->account_code                = $fields['account_code'];
+            $table->account_name                = $fields['account_name'];
+            $table->account_group               = $fields['account_group'];
+            $table->account_default_status      = $fields['account_status'];
+            $table->account_status              = $fields['account_status'];
+            $table->account_type_id             = $fields['account_type_id'];
+            $table->save();
+            DB::commit();
+            return redirect()->route('account.index')->with(['msg' => 'Berhasil Mengubah Data Perkiraan', 'type' => 'success']);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            dd($e);
+            report($e);
+            return redirect()->route('account.edit')->with(['msg' => 'Gagal Mengubah Data Perkiraan', 'type' => 'danger']);
+        }
+    }
+
+    public function deleteAcctAccount($account_id)
+    {
+        try {
+            DB::beginTransaction();
+            AcctAccount::find($account_id)->delete();
+            DB::commit();
+            return redirect()->route('account.index')->with(['msg' => 'Berhasil Menghapus Data Perkiraan', 'type' => 'success']);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            report($e);
+            return redirect()->route('account.index')->with(['msg' => 'Gagal Menghapus Data Perkiraan', 'type' => 'danger']);
+        }
+    }
+}
