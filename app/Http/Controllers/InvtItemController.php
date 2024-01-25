5a73ad486be53d1570fadca7f228fedf458b49ae
amandayusnita <amandayusnita94@gmail.com>
2024-01-25 16:02:51 +0700

    invitem

--- app/Http/Controllers/InvtItemController.php
+++ app/Http/Controllers/InvtItemController.php
@@ -89,8 +89,6 @@ class InvtItemController extends Controller
             'item_code'           => 'required',
             'item_name'           => 'required',
             // 'item_unit_id1'       => 'required',
-        ],[
-            'item_category_id.integer'  => 'Wahana / Merchant Tidak Memiliki Kategori',
             // 'item_unit_id1.required'    => 'Harap Masukkan Satuan 1 (Jika satuan 1 susah dimasukkan tapi masih muncul error ini, maka coba refresh halaman web)'
         ]);
         $warehouse = InvtWarehouse::where('company_id', Auth::user()->company_id)->get();
